049c5aa8b1a40b217ef7638eaaf302cedc013693
Alejandro Freitas <freitassanchezalejandro@gmail.com>
2022-10-11 14:29:27 +0200

    Mejoras añadidas.

--- 3enRaya.php
+++ 3enRaya.php
@@ -1,4 +1,5 @@
 <?php
+
 //array del tablero
 $tablero = array(
     1 => array(
@@ -17,11 +18,14 @@ $tablero = array(
         3 => "-",
     ),
 );
-//variable turno, 0 = jugador x y 1 =jugador o
+
+//variables
 $turno = 0;
 $ganador = false;
-$contador = 0;
-//simplemente imprime el tablero
+$contador = 1;
+$letraJugador = "X";
+
+/*Esta función simplemente imprime el tablero */
 function imprimirTablero()
 {
     global $tablero;
@@ -33,91 +37,102 @@ function imprimirTablero()
     echo "|  {$tablero[3][1]}  |  {$tablero[3][2]}  |  {$tablero[3][3]}  |\n";
     echo "+-----+-----+-----+\n";
 }
-
+/*
+    *Esta función comprueba si la fila y la columna introducidas son validas,
+    *teniendo en cuenta si son numeros, que sean mayores a 1 y menores a 3(las medidas del tablero),
+    *o si ya estan ocupadas.
+    *Devuelve un booleano.
+*/
+function casillaValida($fila, $columna)
+{
+    global $tablero;
+    $error = false;
+    //si no son numeros
+    if (is_numeric($fila) == false || is_numeric($columna) == false) {
+        $error = true;
+        echo "errorm alguno de los parametros  no son un número\n";
+    //si son menores que 3 y mayores que 1
+    } else if ($fila < 1 || $fila > 3 || $columna < 1 || $columna > 3) {
+        $error = true;
+        echo "error, alguno de los parametros es menor que 1 o menor que 3\n";
+    //si no estan ocupadas
+    } else if ($tablero[$fila][$columna] == "X" || $tablero[$fila][$columna] == "O") {
+        $error = true;
+        echo "La casilla ya esta ocupada, elige de nuevo.\n";
+    }
+    return $error;
+}
+/*
+    *Esta función permite elegir la casilla del tablero en la que queremos colocar la ficha.
+    *Si la funcion casillaValida da false, introduce la ficha en la casilla elegida.
+*/
 function elegirCasilla()
 {
-    global $turno, $tablero, $contador;
-    //jugador X
-    if ($turno == 0) {
-        echo "Turno del jugador X: \n";
-        $fila = readline("Introduce la fila: ");
-        $columna = readline("Introduce la columna: ");
-        //comprueba que la fila y la columa sea entre 1 y 3 y que sea un numero
-        if ($fila < 1 || $fila > 3 || $columna < 1 || $columna > 3 || is_numeric($fila) == false || is_numeric($columna) == false) {
-            echo "error, alguno de los parametros es menor que 1 o menor que 3 o no son un número\n";
-        } else {
-            //comprueba que la celda no esté ocupada por otra ficha
-            if ($tablero[$fila][$columna] == "X" || $tablero[$fila][$columna] == "O") {
-                echo "La casilla ya esta ocupada, elige de nuevo.\n";
-            } else {
-                //El valor de la celda pasa a ser X
-                $tablero[$fila][$columna] = "X";
-                //Cambia el turno del jugador
-                $turno = 1;
-                $contador++;
-                imprimirTablero();
-                comprobarVictoria();
-            }
-        }
-        //jugador O
-
-    } else {
-        echo "Turno del jugador O: \n";
-        $fila = readline("Introduce la fila: ");
-        $columna = readline("Introduce la columna: ");
-        //comprueba que la fila y la columa sea entre 1 y 3 y que sea un numero
-        if ($fila < 1 || $fila > 3 || $columna < 1 || $columna > 3 || is_numeric($fila) == false || is_numeric($columna) == false) {
-            echo "error, alguno de los parametros es menor que 1 o menor que 3 o no son un número\n";
-        } else {
-            //comprueba que la celda no esté ocupada por otra ficha
-            if ($tablero[$fila][$columna] == "X" || $tablero[$fila][$columna] == "O") {
-                echo "La casilla Oa esta ocupada, elige de nuevo.\n";
-            } else {
-                //el valor de la celda pasa a ser O
-                $tablero[$fila][$columna] = "O";
-                //cambia el turno del jugador
-                $turno = 0;
-                $contador++;
-                imprimirTablero();
-                comprobarVictoria();
-            }
-        }
+    global $tablero, $contador, $letraJugador;
+    echo "Turno del jugador $letraJugador: \n";
+    //introducir la fila y columna
+    $fila = readline("Introduce la fila: ");
+    $columna = readline("Introduce la columna: ");
+    if (casillaValida($fila, $columna) == false) {
+        //El valor de la celda pasa a ser la letra del jugador
+        $tablero[$fila][$columna] = "$letraJugador";
+        //Cambia el turno del jugador
+        comprobarVictoria();
+        $contador++;
+        //cambia el jugador
+        cambioJugador();
+        imprimirTablero();
     }
 }
+/*
+    *Esta funcion permite el cambio de jugador si el turno es 0, el siguiente turno sera 1, con su respectiva letra,
+    * si es 0 al contrario, será 1.
+*/
+function cambioJugador()
+{
+    global $turno, $letraJugador;
+    if ($turno == 0) {
+        $turno = 1;
+        $letraJugador = "O";
+    } else {
+        $turno = 0;
+        $letraJugador = "X";
+    }
+}
+/*
+    *Esta funcion permite saber si un jugador ha ganado comprobando todas las posibles combinaciones
+    *ganadoras de fichas.
+    *Si ningun jugador consigue ganar la partida termina en empate usando la variable $contador al llegar a 9.
+*/
 function comprobarVictoria()
 {
-    global $tablero, $ganador, $contador;
+    global $tablero, $ganador, $contador, $letraJugador;
     //todas las posibilidades de victoria de cada jugador - | / \
     if (
-        $tablero[1][1] == "X" && $tablero[1][2] == "X" && $tablero[1][3] == "X" ||
-        $tablero[1][1] == "X" && $tablero[2][1] == "X" && $tablero[3][1] == "X" ||
-        $tablero[1][3] == "X" && $tablero[2][3] == "X" && $tablero[3][3] == "X" ||
-        $tablero[3][1] == "X" && $tablero[3][2] == "X" && $tablero[3][3] == "X" ||
-        $tablero[2][1] == "X" && $tablero[2][2] == "X" && $tablero[3][2] == "X" ||
-        $tablero[1][2] == "X" && $tablero[2][2] == "X" && $tablero[3][2] == "X" ||
-        $tablero[1][1] == "X" && $tablero[2][2] == "X" && $tablero[3][3] == "X" ||
-        $tablero[3][1] == "X" && $tablero[2][2] == "X" && $tablero[1][3] == "X"
+        //arriva horizontal
+        $tablero[1][1] == "$letraJugador" && $tablero[1][2] == "$letraJugador" && $tablero[1][3] == "$letraJugador" ||
+        //izquierda vertical
+        $tablero[1][1] == "$letraJugador" && $tablero[2][1] == "$letraJugador" && $tablero[3][1] == "$letraJugador" ||
+        //derecha vertical
+        $tablero[1][3] == "$letraJugador" && $tablero[2][3] == "$letraJugador" && $tablero[3][3] == "$letraJugador" ||
+        //abajo horizontal
+        $tablero[3][1] == "$letraJugador" && $tablero[3][2] == "$letraJugador" && $tablero[3][3] == "$letraJugador" ||
+        //centro horizontal
+        $tablero[2][1] == "$letraJugador" && $tablero[2][2] == "$letraJugador" && $tablero[2][3] == "$letraJugador" ||
+        //centro vertical
+        $tablero[1][2] == "$letraJugador" && $tablero[2][2] == "$letraJugador" && $tablero[3][2] == "$letraJugador" ||
+        ////diagonal
+        $tablero[1][1] == "$letraJugador" && $tablero[2][2] == "$letraJugador" && $tablero[3][3] == "$letraJugador" ||
+        //diagonal
+        $tablero[3][1] == "$letraJugador" && $tablero[2][2] == "$letraJugador" && $tablero[1][3] == "$letraJugador"
     ) {
         //ganador pasa a ser true para que el juego termine
         $ganador = true;
-        echo "La victoria es del jugador X";
-    } else if (
-        $tablero[1][1] == "O" && $tablero[1][2] == "O" && $tablero[1][3] == "O" ||
-        $tablero[1][1] == "O" && $tablero[2][1] == "O" && $tablero[3][1] == "O" ||
-        $tablero[1][3] == "O" && $tablero[2][3] == "O" && $tablero[3][3] == "O" ||
-        $tablero[3][1] == "O" && $tablero[3][2] == "O" && $tablero[3][3] == "O" ||
-        $tablero[2][1] == "O" && $tablero[2][2] == "O" && $tablero[3][2] == "O" ||
-        $tablero[1][2] == "O" && $tablero[2][2] == "O" && $tablero[3][2] == "O" ||
-        $tablero[1][1] == "O" && $tablero[2][2] == "O" && $tablero[3][3] == "O" ||
-        $tablero[3][1] == "O" && $tablero[2][2] == "O" && $tablero[1][3] == "O"
-    ) {
-        //ganador pasa a ser true para que el juego termine
-        $ganador = true;
-        echo "La victoria es del jugador O";
-    }else if ($contador==9){
+        echo "La victoria es del jugador $letraJugador\n";
+    } else if ($contador == 9) {
         //cuando el contador llega a nueve quiere decir que no quedan celdas disponibles, por lo que es empate
         $ganador = true;
-        echo "No quedan celdas, la partida acaba en empate";
+        echo "No quedan celdas, la partida acaba en empate\n";
     }
 }
 
